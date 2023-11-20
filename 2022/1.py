1bc936689a0e79ecbf226c406d087ed747cced99
Chayan Toufan Tabrizi <chayantt@gmail.com>
2023-11-20 22:23:21 +0100

    feat(puzzle): :rocket: 1-2022

--- 2022/1.py
+++ 2022/1.py
@@ -1,1 +1,13 @@
 data = open("data/1.txt").read().split("\n")
+
+sumCalories = []
+currentSum = 0
+for calories in data:
+    if calories == "":
+        sumCalories.append(currentSum)
+        currentSum = 0
+    else:
+        currentSum += int(calories)
+
+print(max(sumCalories))
+print(sum(sorted(sumCalories)[-3:]))
